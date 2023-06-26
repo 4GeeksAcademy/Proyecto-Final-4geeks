f6b7f454e1e1d73b8a328ebdad86cd25e249c0e0
robmab <56076087+robmab@users.noreply.github.com>
2023-06-26 12:36:04 +0000

    Clasificación categorías | Ordenar tabla

--- src/front/js/pages/classification.jsx
+++ src/front/js/pages/classification.jsx
@@ -4,7 +4,9 @@ import { Context } from "../store/appContext";
 
 import "../../styles/classification.css";
 import "../../styles/table.css";
-import { Button, ListGroupItem } from "react-bootstrap";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretUp, faCaretDown } from "@fortawesome/free-solid-svg-icons";
 
 export const Classification = () => {
   useEffect(() => {
@@ -18,7 +20,9 @@ export const Classification = () => {
   const [categorie, setCategorie] = useState({});
 
   const [runners, setRunners] = useState([]);
+  const [sort, setSort] = useState({});
 
+  /* ARRAY OF RUNNERS DEPENDING OF FILTERS/STATES */
   useEffect(() => {
     let aux = [];
     store.trials.map((item) => {
@@ -26,7 +30,7 @@ export const Classification = () => {
         Object.keys(point)[0] === item.tournament &&
         Object.keys(event).length === 0
       ) {
-        item.runners.map((x, xIndex) => {
+        item.runners.map((x) => {
           const filter = aux.filter((y) => y.name === x.name);
           if (filter.length === 0) {
             aux.push(x);
@@ -36,8 +40,8 @@ export const Classification = () => {
 
             aux.splice(index, 1);
 
-            /* ES NECESARIO CREAR UNA REPLICA DEL ITEM X, 
-            PORQUE SI NO ESTARÍA MODIFICANDO DIRECTAMENTE LA STORE */
+            /* ES NECESARIO CREAR UNA RÉPLICA DEL ITEM X, 
+            PORQUE SI NO ESTARÍA MODIFICANDO DIRECTAMENTE LA STORE!! */
             const replicaX = {};
 
             for (const i in x) {
@@ -45,6 +49,16 @@ export const Classification = () => {
               else replicaX[i] = x[i];
             }
             aux.push(replicaX);
+          }
+        });
+      } else if (
+        Object.keys(point)[0] === item.tournament &&
+        Object.keys(event)[0] === item.name &&
+        Object.keys(categorie).length > 0
+      ) {
+        item.runners.map((x) => {
+          if (x.categorie === Object.keys(categorie)[0]) {
+            aux.push(x);
           }
         });
       } else if (
@@ -64,7 +78,57 @@ export const Classification = () => {
 
     setRunners(aux);
     aux = [];
-  }, [point, event]);
+  }, [point, event, categorie]);
+
+  /* SORT FUNCTION FOR TABLE */
+  const handleSort = (e, type) => {
+    const id = e.target.id;
+    const aux = runners;
+    if (sort[type] === true) {
+      setSort({ [id]: false });
+      if (type === "points") {
+        aux.sort((a, b) => {
+          return b.points - a.points;
+        });
+      } else {
+        aux.sort((a, b) => {
+          let fa = a[type].toLowerCase(),
+            fb = b[type].toLowerCase();
+
+          if (fa > fb) {
+            return -1;
+          }
+          if (fa < fb) {
+            return 1;
+          }
+          return 0;
+        });
+      }
+    } else {
+      setSort({ [id]: true });
+
+      if (type === "points") {
+        aux.sort((a, b) => {
+          return a.points - b.points;
+        });
+      } else {
+        aux.sort((a, b) => {
+          let fa = a[type].toLowerCase(),
+            fb = b[type].toLowerCase();
+
+          if (fa < fb) {
+            return -1;
+          }
+          if (fa > fb) {
+            return 1;
+          }
+          return 0;
+        });
+      }
+    }
+
+    setRunners(aux);
+  };
 
   return (
     <div className="page-inside-sideband classification">
@@ -155,33 +219,67 @@ export const Classification = () => {
           </>
         ) : null}
       </div>
-      <div className="table">
-        <table>
-          <thead>
-            <tr>
-              <th>Nombre</th>
-              <th>Equipo</th>
-              <th>Puntos</th>
-            </tr>
-          </thead>
-          <tfoot>
-            <tr>
-              <th colSpan="3">Año: 2023</th>
-            </tr>
-          </tfoot>
-          <tbody>
-            {runners.map((item, index) => {
-              return (
-                <tr key={index}>
-                  <td data-title="Nombre">{item.name}</td>
-                  <td data-title="Equipo">{item.team}</td>
-                  <td data-title="Puntos">{item.points}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
+      {runners.length === 0 ? (
+        <h2>No hay ningún corredor</h2>
+      ) : (
+        <div className="table">
+          <table>
+            <thead>
+              <tr>
+                <th onClick={(e) => handleSort(e, "name")} id="name">
+                  Nombre{" "}
+                  {sort?.name === true ? (
+                    <FontAwesomeIcon icon={faCaretUp} />
+                  ) : sort?.name === false ? (
+                    <FontAwesomeIcon icon={faCaretDown} />
+                  ) : null}
+                </th>
+                <th onClick={(e) => handleSort(e, "team")} id="team">
+                  Equipo{" "}
+                  {sort?.team === true ? (
+                    <FontAwesomeIcon icon={faCaretUp} />
+                  ) : sort?.team === false ? (
+                    <FontAwesomeIcon icon={faCaretDown} />
+                  ) : null}
+                </th>
+                <th onClick={(e) => handleSort(e, "points")} id="points">
+                  Puntos{" "}
+                  {sort?.points === true ? (
+                    <FontAwesomeIcon icon={faCaretUp} />
+                  ) : sort?.points === false ? (
+                    <FontAwesomeIcon icon={faCaretDown} />
+                  ) : null}
+                </th>
+                <th onClick={(e) => handleSort(e, "categorie")} id="categorie">
+                  Categoría{" "}
+                  {sort?.categorie === true ? (
+                    <FontAwesomeIcon icon={faCaretUp} />
+                  ) : sort?.categorie === false ? (
+                    <FontAwesomeIcon icon={faCaretDown} />
+                  ) : null}
+                </th>
+              </tr>
+            </thead>
+            <tfoot>
+              <tr>
+                <th colSpan={Object.keys(runners[0]).length}>Año: 2023</th>
+              </tr>
+            </tfoot>
+            <tbody>
+              {runners.map((item, index) => {
+                return (
+                  <tr key={index}>
+                    <td data-title="Nombre">{item.name}</td>
+                    <td data-title="Equipo">{item.team}</td>
+                    <td data-title="Puntos">{item.points}</td>
+                    <td data-title="Categoría">{item.categorie}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 };
